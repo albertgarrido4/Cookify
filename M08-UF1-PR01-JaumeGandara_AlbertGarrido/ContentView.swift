48e18a9e5bade9b809c3b278b5d9d3250599ef4b
Albert Garrido <algarri2000@gmail.com>
2024-12-12 19:20:18 +0100

    Initial Commit

---
+++ M08-UF1-PR01-JaumeGandara_AlbertGarrido/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M08-UF1-PR01-JaumeGandara_AlbertGarrido
+//
+//  Created by alumne on 12/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M08-UF1-PR01-JaumeGandara_AlbertGarrido/M08_UF1_PR01_JaumeGandara_AlbertGarridoApp.swift
@@ -0,0 +1,17 @@
+//
+//  M08_UF1_PR01_JaumeGandara_AlbertGarridoApp.swift
+//  M08-UF1-PR01-JaumeGandara_AlbertGarrido
+//
+//  Created by alumne on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct M08_UF1_PR01_JaumeGandara_AlbertGarridoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
